22b7cac83f3c5415ab7093fb2731033b53dcc041
sinwoo-kim <shinwoo5676@gmail.com>
2025-01-04 17:09:04 +0900

    Style: record 적용

--- src/main/java/org/example/todo/todo/dto/response/ReadTodoResponseDto.java
+++ src/main/java/org/example/todo/todo/dto/response/ReadTodoResponseDto.java
@@ -1,27 +1,21 @@
 package org.example.todo.todo.dto.response;
 
-import lombok.Getter;
 import org.example.todo.todo.entity.Todo;
 
 import java.time.LocalDateTime;
 
-@Getter
-public class ReadTodoResponseDto {
+public record ReadTodoResponseDto(Long todoId, String title, String contents, LocalDateTime createdAt,
+                                  LocalDateTime modifiedAt) {
 
-    private final Long todoId;
-    private final String username;
-    private final String title;
-    private final String contents;
-    private LocalDateTime createAt;    // 생성 날짜
-    private LocalDateTime modifiedAt;  // 수정 날짜
+    public static ReadTodoResponseDto toDto(Todo todo) {
+        return new ReadTodoResponseDto(
+                todo.getTodoId(),
+                todo.getTitle(),
+                todo.getContents(),
+                todo.getCreateAt(),
+                todo.getModifiedAt()
+        );
 
-    public ReadTodoResponseDto(Todo todo) {
-        this.todoId = todo.getTodoId();
-        this.username = todo.getUsername();
-        this.title = todo.getTitle();
-        this.contents = todo.getContents();
-        this.createAt = todo.getCreateAt();        // BaseEntity에서 상속된 필드
-        this.modifiedAt = todo.getModifiedAt();
     }
 
 }
--- src/main/java/org/example/todo/user/dto/response/UserResponseDto.java
+++ src/main/java/org/example/todo/user/dto/response/UserResponseDto.java
@@ -1,10 +1,8 @@
 package org.example.todo.user.dto.response;
 
-import lombok.Getter;
 import org.example.todo.user.entity.User;
 
 
-@Getter
 public record UserResponseDto(String name, String email) {
 
     public static UserResponseDto toDto(User user) {
